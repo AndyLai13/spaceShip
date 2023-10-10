c95793d36f4dac5bdca863d9c8f124194136f20c
andy.lai <badboyandy13@gmail.com>
2023-10-10 14:00:29 +0800

    處理石頭的旋轉與畫面失真

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ clock = pygame.time.Clock()
 
 background_img = pygame.image.load(os.path.join("img", "background.png")).convert()
 player_img = pygame.image.load(os.path.join("img", "player.png")).convert()
-rock_img = pygame.image.load(os.path.join("img", "rock.png")).convert()
+# rock_img = pygame.image.load(os.path.join("img", "rock.png")).convert()
 bullet_img = pygame.image.load(os.path.join("img", "bullet.png")).convert()
+rock_imgs = []
+for i in range(7):
+    rock_imgs.append(pygame.image.load(os.path.join("img", f"rock{i}.png")).convert())
 
 
 class Player(pygame.sprite.Sprite):
@@ -56,17 +59,29 @@ class Player(pygame.sprite.Sprite):
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = rock_img
-        self.image.set_colorkey(BLACK)
+        self.image_ori = random.choice(rock_imgs)
+        self.image_ori.set_colorkey(BLACK)
+        self.image = self.image_ori.copy()
         self.rect = self.image.get_rect()
-        self.radius = self.rect.width * 0.85/ 2
+        self.radius = self.rect.width * 0.85 / 2
         # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-180, -100)
         self.speedy = random.randrange(2, 10)
         self.speedx = random.randrange(-3, 3)
+        self.total_degree = 0
+        self.rot_degree = random.randrange(-3, 3)
+
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree = self.total_degree % 360
+        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
